beb2df33bc9a41aa5cba8a9952201997784c43d9
unknown <you@examp>
2025-05-28 23:26:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterph.bwaiqag.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPH`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+     const jobCollection =client.db('careerCode').collection('jobs')
+     const applicationCollection = client.db('careerCode').collection('applications')
+    // create job api
+
+    app.get('/jobs', async(req, res) => {
+        const jobs = req.body;
+        const result = await jobCollection.find().toArray()
+        res.send(result)
+    })
+    app.get('/jobs/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query ={
+            _id : new ObjectId(id)
+        }
+        const result = await jobCollection.findOne(query)
+        res.send(result)
+    })
+    // application api
+    app.get('/applications', async(req, res)=>{
+        const email = req.query.email;
+        const query = {
+            applicant : email
+        }
+        const result =await applicationCollection.find(query).toArray();
+        res.send(result)
+    })
+
+
+
+    app.post('/applications', async(req, res) =>{
+        const application = req.body;
+        const result = await applicationCollection.insertOne(application);
+        res.send(result);
+    })
+   
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res)=>{
+    res.send('Career Code Cooking')
+})
+
+app.listen(port, () =>{
+    console.log(`New code is running on port ${port}`)
+})
